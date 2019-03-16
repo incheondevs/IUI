651b37a938532687365daab2bc7a47ba24f94479
RbertKo <myeongsku@gmail.com>
2019-03-16 12:18:40 +0900

    resolved

--- src/components/button/button.container.js
+++ src/components/button/button.container.js
@@ -1,14 +1,20 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Button from './button.presentational';
 
 let test = "Hello World";
 
-let ButtonWrapper = (props)=> (WrapperComponent) => {
+function ButtonWrapper(data, WrapperComponent) {
     //data 처리 과정, 통신을 통해서 받아온 데이터를 전달해준다던지
     // console.log(props);
-    return <WrapperComponent {...props} ></WrapperComponent>
+    return class extends Component {
+        render() {
+            return (
+                <WrapperComponent {...data}></WrapperComponent>
+            )
+        }
+    }
+    // return <WrapperComponent {...props} ></WrapperComponent>
 }
-console.log(ButtonWrapper({ test }, Button));
 
 // export let _Button = ButtonWrapper({ test })(Button);
-export default ButtonWrapper({ test })(Button);
+export default ButtonWrapper({ test }, Button);;
